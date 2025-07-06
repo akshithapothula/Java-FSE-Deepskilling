5185ee5d2c612ac0bd71b6953d3daa8fe3de381d
akshithapothula <akshithapothula2@gmail.com>
2025-07-06 21:31:14 +0530

    uploading ex-4

---
+++ Week-3_Hands On/Spring Core Maven/Exercise-4/src/main/java/com/library/LibraryApp.java
@@ -0,0 +1,7 @@
+package com.library;
+
+public class LibraryApp {
+    public static void main(String[] args) {
+        System.out.println("LibraryManagement Maven project is set up successfully.");
+    }
+}
